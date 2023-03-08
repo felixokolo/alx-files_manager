25f735abaac1564718b781336baf48f25a6f5c62
felixokolo <pheelix.info@gmail.com>
2023-03-08 10:56:29 +0100

    additions

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,5 +1,7 @@
 import RedisClient from '../utils/redis';
 import dbClient from '../utils/db';
+import { v4 as uuid } from 'uuid';
+const fs = require('fs');
 
 class FilesController {
   static async postUpload(request, response) {
@@ -19,19 +21,60 @@ class FilesController {
       const name = body.name;
       const type = body.type;
       const data = body.data;
-      const parentId = body.parentId;
+      const isPublic = request.body.isPublic || false;
+      let parentId = request.body.parentId || 0;
+      parentId = parentId === '0' ? 0 : parentId;
       if (!name) return response.status(400).send({error: 'Missing name'});
-      if (!type) return response.status(400).send({error: 'Missing type'});
+      if (!type || !['folder', 'file', 'image'].includes(type)) return response.status(400).send({error: 'Missing type'});
       if (!data && type !== 'folder') return response.status(400).send({error: 'Missing data'});
-      if (!!parentId) {
+      if (parentId !== 0) {
         const parent = dbClient.files.findOne({id: parentId});
         if (!parent) return response.status(400).send({error: 'Parent not found'});
         if (parent.type !== 'folder') return response.status(400).send({error: 'Parent is not a folder'});
       }
-      const doc = {userId: user._id};
+      const doc = {userId: user._id,
+        name: name,
+        type: type,
+        isPublic: isPublic,
+        parentId: parentId,};
+
       if (type === 'folder'){
-        const _id = dbClient.files.insertOne(doc);
+        const _id = await dbClient.files.insertOne(doc);
+        return response.status(201).send({id: doc._id, ...doc});
       }
+
+      const pathDir = process.env.FOLDER_PATH || '/tmp/files_manager';
+    const fileUuid = uuid();
+
+    const buff = Buffer.from(data, 'base64');
+    const pathFile = `${pathDir}/${fileUuid}`;
+
+    fs.mkdir(pathDir, { recursive: true }, (error) => {
+      if (error) return response.status(400).send({ error: error.message });
+      return true;
+    });
+
+    await fs.writeFile(pathFile, buff, (error) => {
+      if (error) return response.status(400).send({ error: error.message });
+      return true;
+    });
+
+    doc.localPath = pathFile;
+    await dbClient.files.insertOne(doc);
+
+    /* fileQueue.add({
+      userId: doc.userId,
+      fileId: doc._id,
+    }); */
+
+    return response.status(201).send({
+      id: doc._id,
+      userId: doc.userId,
+      name: doc.name,
+      type: doc.type,
+      isPublic: doc.isPublic,
+      parentId: doc.parentId,
+    });
     }
   }
 }
